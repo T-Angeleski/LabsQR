1d640527c19ae530bb026c98fa936951a64b777e
Teodor Angeleski <teodorangeleski@gmail.com>
2024-12-19 23:45:07 +0100

    feat: Setup skeleton for frontend and backend, setup docker compose for backend

---
+++ backend/src/main/java/mk/ukim/finki/LabsProject/LabsProjectApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.LabsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabsProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/java/mk/ukim/finki/LabsProject/controller/IndexController.java
@@ -0,0 +1,14 @@
+package mk.ukim.finki.LabsProject.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String getIndexPage() {
+        return "Hello from the index page!";
+    }
+}
